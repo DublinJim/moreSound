38faa4ffaa43948506e4bb70958fa1c9c3327e94
James Keogh <jkeogh@adga.ca>
2022-04-13 14:17:05 -0400

    init commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Mixer;
+
+public class Main {
+  public static Mixer mixer;
+
+  public static void main(String[] args) {
+
+    System.out.println("Begin");
+
+    soundSystemInfo();
+  }
+
+  private static void soundSystemInfo() {
+    Mixer.Info[] pcSoundInfo = AudioSystem.getMixerInfo();
+    System.out.println(pcSoundInfo[0].getName());
+
+    String headsetType = pcSoundInfo[0].getName();
+
+    if (pcSoundInfo[0].getName().equals("Port Headset Earphone (Microsoft Lif")) {
+      System.out.println("------------------headset-----------------");
+      //
+    }
+  }
+}
